f5503d778a8437a114d4934f3d164a2aa3c008a7
PSLPrabudassDurai <prabudass_durai@persistent.com>
2023-11-27 13:50:23 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SuperMarket_ShoppingCart.Models;
+using SuperMarket_ShoppingCart.Shared.Models;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+using static System.Net.WebRequestMethods;
+
+namespace SuperMarket_ShoppingCart.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+        string UserName = "abc";
+        ShoppingDetails SD = new ShoppingDetails();
+        string Messages = "";
+        
+        int shopID = 0;
+
+
+        [HttpGet]
+        public async Task<List<CartItemDetails>> ItemDetails()
+        {
+            List<CartItemDetails>? myShopList = new List<CartItemDetails>();
+            string url = "/api/ShoppingDetails/";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                var CartItems = response.Content.ReadAsStringAsync().Result;
+                myShopList = JsonConvert.DeserializeObject<List<CartItemDetails>>(CartItems);
+                return myShopList;
+            }
+        } 
+    }
+}
+
---
+++ Controllers/ShoppingController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SuperMarket_ShoppingCart.Shared.Models;
+
+namespace SuperMarket_ShoppingCart.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async void ShoppingDetails(ShoppingDetails SD)
+        {
+            var json = JsonConvert.SerializeObject(SD, Formatting.Indented);
+            var stringContent = new StringContent(json);            
+            string url = "/api/ShoppingDetails/";
+            using (HttpClient client = new HttpClient())
+            {
+                await client.PostAsync(url, stringContent);
+            }
+        }
+    }
+}
